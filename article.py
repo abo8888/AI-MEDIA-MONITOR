2407e7b1f3fffc331e246e3aee2ed90767902e0b
abo8888 <168743410+abo8888@users.noreply.github.com>
2025-02-17 13:47:30 +0100

    article.py

--- article.py
+++ article.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class Article(db.Model):
     __tablename__ = "articles"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     image = db.Column(db.String(255), nullable=True)
